e2e1a6cc777dfd8dce48517e93dcf2f118e85cfc
JoshSutcliffe <joshclement12@gmail.com>
2016-09-05 21:18:04 +0100

    hacked asset pipeline added nodemon

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
 // set up web server
+var express = require('express');
+var app = express();
+var bodyParser = require("body-parser");
+
+// routes
+require('./app/routes.js')(app);
+
+// listen (start app with node server.js)
+app.listen(3000, function() {
+  console.log("server going");
+})
